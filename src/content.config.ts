a6b05a5f4bb835e9399c3b8f73874ba2fff91d86
Yaoyang-Liu <3987966521@qq.com>
2025-10-22 14:53:14 +0800

    big change

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,17 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
+export const collections = {
+	work: defineCollection({
+		// Load Markdown files in the src/content/work directory.
+		loader: glob({ base: './src/content/work', pattern: '**/*.md' }),
+		schema: z.object({
 			title: z.string(),
 			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
 		}),
-});
-
-export const collections = { blog };
+	}),
+};
